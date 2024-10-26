f8b284bd901d5a748d42109d4a9c4ffd788864a7
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-10-25 21:48:19 -0500

    Agregando clase Padre2-Jesus Rodriguez Echenique

--- Assets/SCRIPTS/BaseObstacle.cs
+++ Assets/SCRIPTS/BaseObstacle.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class BaseObstacle : MonoBehaviour
 {
-   
+    public bool isCollectible;
+    public int damage;
+    public int scoreValue;
+    public string obstacleType;
+
+    
+    public virtual void Interact()
+    {
+        Debug.Log($"{obstacleType} interactuado.");
+    }
+
+    public void CauseDamage(PlayerCharacter player)
+    {
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+            Debug.Log($"El jugador recibió {damage} de daño por el {obstacleType}.");
+        }
+    }
+
+    public void Collect()
+    {
+        if (isCollectible)
+        {
+            
+            Debug.Log($"{obstacleType} recogido. Puntos: {scoreValue}.");
+            Destroy(gameObject); 
+        }
+    }
+
+    public bool IsActive()
+    {
+        return gameObject.activeInHierarchy;
+    }
+
+    public void SetActive(bool isActive)
+    {
+        gameObject.SetActive(isActive);
+    }
 }
