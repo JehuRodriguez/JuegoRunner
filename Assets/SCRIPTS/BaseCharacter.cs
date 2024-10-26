cd493a0ed31b156c2c502908c9de4af5f4994af1
Jesus Rodriguez Echenique <j.eh.u14@hotmail.com>
2024-10-25 20:06:04 -0500

    Agregando clase Padre1-Jesus Rodriguez Echenique

---
+++ Assets/SCRIPTS/BaseCharacter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseCharacter : MonoBehaviour
+{
+    public float speed = 5f;
+    public int life = 3;
+    public Text lifeText; 
+    public GameObject gameOverPanel; 
+
+    protected virtual void Move()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            life--;
+            UpdateLifeUI();
+            CheckGameOver();
+        }
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            life--;
+            UpdateLifeUI();
+            CheckGameOver();
+        }
+    }
+
+    private void UpdateLifeUI()
+    {
+        lifeText.text = "Vida: " + life; 
+    }
+
+    private void CheckGameOver()
+    {
+        if (life <= 0)
+        {
+            
+            gameOverPanel.SetActive(true);
+            Time.timeScale = 0; 
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
+}
